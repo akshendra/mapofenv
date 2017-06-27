8937f7e2fe907a9e64161821e66a33ac2d7b58d9
Akshendra Pratap Singh <akshendra521994@gmail.com>
2017-06-28 04:43:03 +0530

    nested objects

--- tests/peg.spec.js
+++ tests/peg.spec.js
@@ -3,7 +3,7 @@
  * @Author: Akshendra Pratap Singh
  * @Date: 2017-06-27 23:29:25
  * @Last Modified by: Akshendra Pratap Singh
- * @Last Modified time: 2017-06-28 00:49:41
+ * @Last Modified time: 2017-06-28 04:22:18
  */
 
 const expect = require('chai').expect;
@@ -13,18 +13,76 @@ const parser = require('../lib/peg');
 describe('Peg', () => {
   it('Parse simple value', () => {
     const string = `
-name: String
-username: String
+db: Object
+  host: String
+  port: Integer
 `;
 
-    process.env.USERNAME = 'pratap';
-    process.env.NAME = 'akshendra';
+    process.env.DB_HOST = '127.0.0.1';
+    process.env.DB_PORT = '1234';
 
     const result = parser.parse(string);
-    // console.log(result);
+
     expect(result).to.deep.equal({
-      name: 'akshendra',
-      username: 'pratap',
+      db: {
+        port: 1234,
+        host: '127.0.0.1',
+      },
+    });
+  });
+
+  it('parse multiple objects', () => {
+    const string = `
+db: Object
+  host: String
+  port: Integer
+secret: String
+redis: Object
+  host: String
+  port: Integer
+`;
+
+    process.env.DB_HOST = '127.0.0.1';
+    process.env.DB_PORT = '1234';
+    process.env.SECRET = 'awesomeness';
+    process.env.REDIS_HOST = '127.0.0.2';
+    process.env.REDIS_PORT = '1235';
+
+    const result = parser.parse(string);
+
+    expect(result).to.deep.equal({
+      db: {
+        port: 1234,
+        host: '127.0.0.1',
+      },
+      secret: 'awesomeness',
+      redis: {
+        port: 1235,
+        host: '127.0.0.2',
+      },
+    });
+  });
+
+  it('should be able to parse double object', () => {
+    const string = `
+one: Object
+  two: Object
+    three: String
+    four: String
+`;
+
+    process.env.ONE_TWO_THREE = 'tree';
+    process.env.ONE_TWO_FOUR = 'four';
+
+    const result = parser.parse(string);
+
+    expect(result).to.deep.equal(result, {
+      one: {
+        two: {
+          three: 'three',
+          four: 'four',
+        },
+      },
     });
   });
 });
